c51a73e2a037f5ce38fef9fb0a0af549e55bc7da
v16s <shankar.vignesh@gmail.com>
2019-09-28 18:46:13 +0530

    working contract(at least on remix :))

--- server/contracts/splitter.sol
+++ server/contracts/splitter.sol
@@ -14,66 +14,61 @@ contract Splitter {
     address caller;
     mapping(address => PendingEntity) pending;
 
-    constructor(address[] memory participants, address payable receiver, uint8[] memory values) payable public {
+    constructor(address[] memory participants, address payable receiver, uint256[] memory values) public payable {
         caller = msg.sender;
         parties = participants;
-        for(uint i=0;i<participants.length;i++){
+        for(uint i = 0;i < participants.length ; i++){
             paid[participants[i]].value = values[i];
         }
-        
         if(participants.length < 10){
             fetchSplits();
         }
-        receiver.transfer(msg.value);
-        
+        send(receiver);
     }
-    event Request(address payee, uint8 val);
-    event Resolve(address payee, uint8 val);
+    event Request(address payee, uint256 val);
+    event Resolve(address payee, uint256 val);
+    function send(address payable receiver) public payable {
+        receiver.transfer(msg.value);
+    }
     function fetchSplits() private {
         uint256 total = 0;
         uint256 negative_total = 0;
-        for(uint8 i=0;i<parties.length;i++){
-            total+=uint256(paid[parties[i]].value);
+        for(uint8 i = 0;i < parties.length;i++){
+            total += uint256(paid[parties[i]].value);
         }
 
         uint256 part = uint256(total)/parties.length;
-        for(uint8 i=0;i<parties.length;i++){
+        for(uint8 i = 0; i < parties.length;i++){
             int256 val = int256(part) - int256(paid[parties[i]].value);
             pending[parties[i]].value = val;
-            
             if(val >= 0) {
                 pending[parties[i]].percentage = 0;
             } else {
-                negative_total+=uint32(val * -1);
+                negative_total += uint256(val * -1);
                 pending[parties[i]].percentage = 1;
             }
         }
-        for(uint8 i=0;i<parties.length;i++){
+        for(uint8 i = 0;i < parties.length;i++){
             int256 val = pending[parties[i]].value;
             if(val < 0) {
-                uint8 percentage = uint8((uint32(val * -1) * 100)/ negative_total);
+                uint8 percentage = uint8((uint32(val * -1) * 100) / negative_total);
                 pending[parties[i]].percentage = percentage;
-            }
-            if(val > 0) {
-                emit Request(parties[i], uint8(val * -1));
+                emit Request(parties[i], uint256(val * -1));
             }
         }
 
     }
-    function resolve() payable public {
+    function resolve() public payable {
         if(parties.length < 10){
             pending[msg.sender].value = pending[msg.sender].value - int256(msg.value);
-            for(uint8 i=0;i<parties.length;i++) {
+            for(uint8 i = 0;i < parties.length;i++) {
                 if(pending[parties[i]].percentage > 0) {
                     address payable en_addr = pending[parties[i]].account;
-                    uint8 val= uint8(pending[parties[i]].percentage * msg.value / 100);
+                    uint256 val = uint256(pending[parties[i]].percentage * msg.value / 100);
                     en_addr.transfer(val);
                     emit Resolve(en_addr, val);
                 }
             }
         }
     }
-    /// Give $(toVoter) the right to vote on this ballot.
-    /// May only be called by $(chairperson).
-    
 }
