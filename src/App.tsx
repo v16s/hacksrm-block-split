66d46930d3bd89f649845829cd116e21ba616513
shrib11 <banerjeeshrirup10@gmail.com>
2019-09-28 15:58:38 +0530

    formView - InProgress

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
 import './App.css';
 
 const App: React.FC = () => {
+  const [fields, setFields] = useState([{ value: null }]);
+
+  function handleChange(i, event) {
+    const values = [...fields];
+    values[i].value = event.target.value;
+    setFields(values);
+  }
+
+  function handleAdd() {
+    const values = [...fields];
+    values.push({ value: null });
+    setFields(values);
+  }
+
+  function handleRemove(i) {
+    const values = [...fields];
+    values.splice(i, 1);
+    setFields(values);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {fields.map((field, idx) => {
+        return (
+          <div key={`${field}-${idx}`}>
+            <input
+              type='text'
+              placeholder='Enter text'
+              value={field.value || ''}
+              onChange={e => handleChange(idx, e)}
+            />
+            <button type='button' onClick={() => handleRemove(idx)}>
+              x
+            </button>
+            <br></br>
+            <button type='button' onClick={() => handleAdd()}>
+              Add new user
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
